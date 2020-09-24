4c731db5f589a80324318c3aa68b83a59f2878c9
Tatiane Aguirres Nogueira <aguirres.tati@gmail.com>
2020-09-24 11:45:11 +0200

    :sparkles: add initial main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
