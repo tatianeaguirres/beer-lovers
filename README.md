05d547be5fac0a9eb69b429025c1e1075bdf79b0
Tatiane Aguirres Nogueira <aguirres.tati@gmail.com>
2020-09-24 11:20:24 +0200

    :tada: initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# BeerLoversApp
+# 🍺 BeerLoversApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.7.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'beer-lovers-app'`, () => {
+  it(`should have as title 'BeerLoversApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('beer-lovers-app');
+    expect(app.title).toEqual('BeerLoversApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('beer-lovers-app app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('BeerLoversApp app is running!');
   });
 });
