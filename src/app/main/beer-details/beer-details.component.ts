bb244929c1c5ef185d840fb86473ad0d89c5d0a0
Tatiane Aguirres Nogueira <aguirres.tati@gmail.com>
2020-10-13 14:28:29 +0200

    :art: update beer details structure and style

--- src/app/main/beer-details/beer-details.component.ts
+++ src/app/main/beer-details/beer-details.component.ts
@@ -15,6 +15,22 @@ export class BeerDetailsComponent implements OnInit {
   beers: Beer[] = [];
   error = null;
   isFetching = false;
+  beerColor: any = null;
+  beerColorName: any = null;
+  beerColorTable = [
+    ['#FFFC46', 2, 'Pale Straw'],
+    ['#FFE93D', 3, 'Straw'],
+    ['#FED849', 4, 'Pale Gold'],
+    ['#FFA847', 6, 'Deep Gold'],
+    ['#F49F44', 9, 'Pale Amber'],
+    ['#D77F59', 12, 'Medium Amber'],
+    ['#94523A', 15, 'Deep Amber'],
+    ['#804541', 18, 'Amber-Brown'],
+    ['#5B342F', 20, 'Brown'],
+    ['#4C3B2B', 24, 'Ruby Brown'],
+    ['#38302E', 30, 'Deep Brown'],
+    ['#31302C', 40, 'Black']
+  ];
 
   constructor(private route: ActivatedRoute, private beersService: BeersService) { }
 
@@ -29,12 +45,23 @@ export class BeerDetailsComponent implements OnInit {
       beers => {
         this.isFetching = false;
         this.beers = beers;
+        if (this.beers[0].srm) {
+          this.onDeterminesBeerColor(this.beers[0].srm);
+        }
       },
       error => {
         this.isFetching = false;
         this.error = error.message;
       }
     );
+  }
+
+  onDeterminesBeerColor(srm: number) {
+    this.beerColorTable.sort((a: any, b: any) => {
+      return Math.abs(a[1] - srm) - Math.abs(b[1] - srm);
+    });
+    this.beerColor = this.beerColorTable[0][0];
+    this.beerColorName = this.beerColorTable[0][2];
   }
 
 }
