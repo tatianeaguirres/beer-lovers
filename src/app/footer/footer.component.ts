f253113e427542bda8e18f386a572d0dcf68d40b
Tatiane Aguirres Nogueira <aguirres.tati@gmail.com>
2020-10-13 14:27:34 +0200

    :art: update footer structure and style

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  date = new Date().getFullYear();
 
   constructor() { }
 
